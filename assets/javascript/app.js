85c9dfefd0005f29cc6785ab4c9faeea4f1090c4
JoeySuc89 <jsucameli1@gmail.com>
2019-03-23 15:16:09 -0400

    created all the necessary files and put a question array inside my js doc

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+var myQuestions = [
+   {
+     question: "What doesn't the fox say?",
+     answers: [a:"Hatee Ho", b:"dingeringeding", c:"Joff-tchoff", d:"woof"],
+     correctAnswer: "d",
+  },
+  {  question: 'What did the "D" in "D-Day" stand for?',
+     answers: [a:"doom", b:"day", c:"Dwight", d:"Dunkirk"],
+     correctAnswer: "b"
+  },
+  {  question: "Which color does not belong in the rainbow?",
+     answers: [a:"Orange", b:"Green", c:"Purple", d:"Indigo"],
+     correctAnswer: "c"
+  },
+  {  question: "Who wrote a series of novels about orcs, hobbits, goblins and elves?",
+     answers: [a:"Oscar Wilde", b:"J.R.R. Tolkien", c:"R.L. Stein", d:"J.K. Rowling"],
+     correctAnswer: "b"
+  },
+  {  question: "What is the bluebird a symbol of?",
+     answers: [a:"Hatred", b:"Happiness", c:"Sadness", d:"Love"],
+     correctAnswer: "b"
+  },
+  {  question: "What is rum distilled from?",
+     answers: [a:"Sugar Cane", b:"Ginger", c:"Cocaine", d:"Flour"],
+     correctAnswer: "a"
+  },
+  {  question: "How many symphonies ddi Beethoven compose?",
+     answers: [a:"seven", b:"thirty two", c:"nine", d:"all of them"],
+     correctAnswer: "c"
+  },
+  {  question: "What is the Chinese game played with small tiles called?",
+  answers: [a:"Tiles", b:"Rummy Cube", c:"Gin", d:"Mah-Jong"],
+  correctAnswer: "d"
+},
+];
+console.log(myQuestions);
